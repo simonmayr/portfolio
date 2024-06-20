365ef31ef07312dcc14d433bdbdd82817b62cb2c
Simon Mayr <simon.mayr@holzweg2.com>
2024-06-20 13:12:01 +0200

    fix domain check

--- src/routes/api/domain-trace/+server.ts
+++ src/routes/api/domain-trace/+server.ts
@@ -1,14 +1,14 @@
 import { error } from '@sveltejs/kit';
 import * as dns from 'dns'; // Import the dns module
+import tldjs from 'tldjs';
+
 const dnsPromises = dns.promises; // Use the Promise-based API of the dns module
 
 export async function GET({ url }) {
-	const domain = String(url.searchParams.get('domain'));
+	const domainParam = String(url.searchParams.get('domain'));
+	const domain = tldjs.getDomain(domainParam);
 
-	const domainValidationRegex =
-		/^(((http|https):\/\/|)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?)$/;
-
-	if (domainValidationRegex.test(domain) === false) {
+	if (!domain) {
 		error(400, 'Invalid domain name');
 	}
 
